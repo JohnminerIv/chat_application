cadb0a6f641fc711c11f822a644b0dfa2ab7fb3f
JohnminerIv <john.douglas.miner.iv@gmail.com>
2021-05-07 22:47:15 -0800

    Implemented changing channels

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const exphbs = require('express-handlebars');
 //Socket.io
 const io = require('socket.io')(server);
 let onlineUsers = {};
+//Save the channels in this object.
+let channels = {"General" : []};
+
 io.on("connection", (socket) => {
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
-})
+  // Make sure to send the channels to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+});
 
 //Establish your public folder
 app.use('/public', express.static('public')
--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,25 @@ $(document).ready(()=>{
   let currentUser;
   socket.emit('get online users');
 
+  socket.emit('user changed channel', "General");
+
+  //Users can change the channel by clicking on its name.
+  $(document).on('click', '.channel', (e)=>{
+    let newChannel = e.target.textContent;
+    socket.emit('user changed channel', newChannel);
+  });
+
+
+  $('#new-channel-btn').click( () => {
+    let newChannel = $('#new-channel-input').val();
+
+    if(newChannel.length > 0){
+      // Emit the new channel to the server
+      socket.emit('new channel', newChannel);
+      $('#new-channel-input').val("");
+    }
+  });
+
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
     if($('#username-input').val().length > 0){
@@ -17,22 +36,23 @@ $(document).ready(()=>{
     }
   });
 
-  $('#send-chat-btn').click((e) => {
-    e.preventDefault();
-    // Get the message text value
-    let message = $('#chat-input').val();
-    // Make sure it's not empty
-    if(message.length > 0){
-      // Emit the message with the current user to the server
-      socket.emit('new message', {
-        sender : currentUser,
-        message : message,
-      });
-      $('#chat-input').val("");
-    }
-  });
+$('#send-chat-btn').click((e) => {
+  e.preventDefault();
+  // Get the client's channel
+  let channel = $('.channel-current').text();
+  let message = $('#chat-input').val();
+  if(message.length > 0) {
+    socket.emit('new message', {
+      sender : currentUser,
+      message : message,
+      //Send the channel over to the server
+      channel : channel
+    });
+    $('#chat-input').val("");
+  }
+});
 
-  //socket listeners
+//socket listeners
   socket.on('new user', (username) => {
     console.log(`${username} has joined the chat`);
     $('.users-online').append(`<div class="user-online">${username}</div>`);
@@ -54,12 +74,38 @@ socket.on('get online users', (onlineUsers) => {
   }
 })
 
-//Output the new message
 socket.on('new message', (data) => {
-  $('.message-container').append(`
-    <div class="message">
-      <p class="message-user">${data.sender}: </p>
-      <p class="message-text">${data.message}</p>
-    </div>
-  `);
-})
+  //Only append the message if the user is currently in that channel
+  let currentChannel = $('.channel-current').text();
+  if(currentChannel == data.channel) {
+    $('.message-container').append(`
+      <div class="message">
+        <p class="message-user">${data.sender}: </p>
+        <p class="message-text">${data.message}</p>
+      </div>
+    `);
+  }
+});
+
+// Add the new channel to the channels list (Fires for all clients)
+socket.on('new channel', (newChannel) => {
+  $('.channels').append(`<div class="channel">${newChannel}</div>`);
+});
+
+// Make the channel joined the current channel. Then load the messages.
+// This only fires for the client who made the channel.
+socket.on('user changed channel', (data) => {
+  $('.channel-current').addClass('channel');
+  $('.channel-current').removeClass('channel-current');
+  $(`.channel:contains('${data.channel}')`).addClass('channel-current');
+  $('.channel-current').removeClass('channel');
+  $('.message').remove();
+  data.messages.forEach((message) => {
+    $('.message-container').append(`
+      <div class="message">
+        <p class="message-user">${message.sender}: </p>
+        <p class="message-text">${message.message}</p>
+      </div>
+    `);
+  });
+});
